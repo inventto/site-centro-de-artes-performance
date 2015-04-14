c0e2a966ab676e6356b4dca0d4ce2e53be512105
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2015-04-14 09:11:33 -0300

    Testes de autenticação com o google calendar.

--- performance/app/assets/javascripts/agenda-eventos.js
+++ performance/app/assets/javascripts/agenda-eventos.js
@@ -1,20 +1,44 @@
 var CLIENT_ID = '186026638324-0l2m013uh26dlvgemd2i7tc994ua9fma.apps.googleusercontent.com';
 var apiKey = 'AIzaSyDC9f1UQjrU6uxvrNPB4Ze29sZt5SCnOts';
 calendarid = '3sv3aefg65slkecmgb1omfirc8@group.calendar.google.com';
-  
-      $.ajax({
+    //url: encodeURI('https://www.googleapis.com/calendar/v3/calendars/' + calendarid+ '/events?key=' + apiKey),
+url_feed = 'https://www.google.com/calendar/feeds/'+calendarid+'/public/full';
+    google.load("gdata", 2);
+    function setupService() {
+      var service = new google.gdata.calendar.CalendarService('Agenda');
+      return service;
+    }
+
+    function getFeed(){
+      service = setupService();
+      service.getEventsFeed(url_feed);
+    }
+
+    $.ajax({
        type: 'GET',
-       url: encodeURI('https://www.googleapis.com/calendar/v3/calendars/' + calendarid+ '/events?key=' + apiKey),
-       dataType: 'json',
+       url: 'https://www.google.com/calendar/feeds/'+calendarid+'/public/basic?singleEvents=true&key='+apiKey,
+       headers:'Access-Control-Allow-Origin',
+       crossDomain: true,
+       dataType: 'jsonp',
+       ProcessData: true,
        success: function (response) {
-        console.log("SUCCESS",response);
+        console.log("SUCCESS",response);       
        }, 
        error: function (response) {
         console.log("ERROR",response);
        }
      });
-/*$.getJSON("https://www.google.com/calendar/feeds/3sv3aefg65slkecmgb1omfirc8%40group.calendar.google.com/private-a1159206bcc1fae12b66e2f602484b96/full?alt=json-in-script&callback=?&orderby=updated&max-results=500&singleevents=true&sortorder=descending&futureevents=false",
-  function(agenda) {
+    //https://www.googleapis.com/calendar/v3/calendars/"+ calendarid +"/
+    //events?key=private-a1159206bcc1fae12b66e2f602484b96/
+    //full?alt=json-in-script&callback=?&orderby=updated&max-results=500&singleevents=true&sortorder=descending&futureevents=false
+    function onload(init) {
+      gapi.load('auth', init);
+      oauthToken = gapi.auth.getToken();
+      alert(oauthToken);
+    }
+    
+  /*$.getJSON('https://www.google.com/calendar/feeds/'+calendarid+'/public/basic?singleEvents=true&key='+apiKey,
+  function(agenda) {    
     if (agenda.feed.entry == undefined) {
       return;
     }
@@ -31,11 +55,11 @@ calendarid = '3sv3aefg65slkecmgb1omfirc8@group.calendar.google.com';
         content = agenda.feed.entry[i].content.$t;
         content = content.replace("\n", "<br/>");
         title = agenda.feed.entry[i].title.$t;
-        */
-        //startTime = data.startTime.replace(/T.*/, "").split("-").reverse().join("/");
-        //endTime = data.endTime.replace(/T.*/, "").split("-").reverse().join("/");
-        //color = "#000";
-        /*content = content.replace(/(http[^ ]+.(jpg|jpeg|gif|png))/mg, "<img src='\$1' style='float:left;margin:10px;width: 906px;'/>");
+        
+      //  startTime = data.startTime.replace(/T.*/ //, "").split("-").reverse().join("/");
+      //  endTime = data.endTime.replace(/T.*/, "").split("-").reverse().join("/");
+      /*  color = "#000";
+        content = content.replace(/(http[^ ]+.(jpg|jpeg|gif|png))/mg, "<img src='\$1' style='float:left;margin:10px;width: 906px;'/>");
         content = content.replace(/download\(([^;]+);"([^"]*)"\)/, "<a href='\$1' title='Download' target='_blank'><img src='https://cdn1.iconfinder.com/data/icons/pretty_office_3/32/Package-Download.png' style='margin:0 10px -10px 0;' />\$2</a>");
         if (startTime == endTime) {
           $('#accordion-container').append("<div id='" + title.replace(/ /g, "-") + "' class='accordion-header'><h2>" + startTime + " - " + title.toUpperCase() + "</h2></div><div class='accordion-content'>" + content + "<p>" + where + "</p></div>");
